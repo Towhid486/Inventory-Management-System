689fed626bca82650be323eee6600d380b80619e
Towhid486 <developertowhid@gmail.com>
2025-06-28 17:12:10 +0600

    JWT Updated 2

--- inventory-backend/src/middlewares/AuthVerification.js
+++ inventory-backend/src/middlewares/AuthVerification.js
@@ -4,8 +4,10 @@ module.exports = (req, res, next) => {
     let JWT_KEY = process.env.JWT_SECRET_KEY
     jwt.verify(Token, JWT_KEY, (err, decoded) => {
         if(err){
-            console.log(err)
-            res.status(401).json({status:false, message:"Unauthorized"})
+            if (err.name === 'TokenExpiredError') {
+                return res.status(401).json({ status: false, message: "Token expired" });
+            }
+            return res.status(401).json({status:false, message:"Unauthorized"})
         }else{
             let email = decoded['data'];
             req.headers.email = email;
